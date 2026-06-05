25edc0ca2bd421061d60eea4bdbaa11bb015cdc6
Dmitriy <babygreenyoda@yandex.ru>
2026-06-05 11:29:50 +0800

    infra: update docker-compose and add testing infrastructure

--- lib/autoservice_core/src/autoservice_core/models.py
+++ lib/autoservice_core/src/autoservice_core/models.py
@@ -1,10 +1,13 @@
 from pydantic import BaseModel
+from typing import Optional
 
 class Part(BaseModel):
     id: int
     name: str
-    count: int
+    article: str
     price: float
+    quantity: int
+    category: Optional[str] = None
 
 class OrderItem(BaseModel):
     part_id: int
